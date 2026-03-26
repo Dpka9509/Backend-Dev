3e62627c1f75f2ef6dfde94f552faad2d78a18ab
Deepika Srivastava <dpka9509@gmain.com>
2026-03-26 09:19:42 +0530

    [Deepika] adds: updated code with bcrypt

--- MONGODBCRUD2/model/UserSchema.js
+++ MONGODBCRUD2/model/UserSchema.js
@@ -23,6 +23,4 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const user = mongoose.model("User", userSchema);
-
-export default user;
+export default mongoose.models.User || mongoose.model("User", userSchema);
--- MONGODBCRUD2/server.js
+++ MONGODBCRUD2/server.js
@@ -2,6 +2,7 @@ import connectDb from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
 import router from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDb();
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api", router);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
